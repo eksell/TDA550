2326777f95d3248a587ba5ee637196baf9ac6edc
Eksell <antoneksell@hotmail.com>
2013-11-14 16:13:19 +0100

    Added files to Repo

---
+++ TDA550/src/lab_1_task/GameFactory.java
@@ -0,0 +1,35 @@
+package lab_1_task;
+
+/**
+ * Factory class for available games.
+ */
+public class GameFactory implements IGameFactory {
+
+	/**
+	 * Returns an array with names of games this factory can create. Used by GUI
+	 * list availible games.
+	 */
+	
+	
+	@Override
+	public String[] getGameNames() {
+		return new String[] { "Gold","Snake" }; //+Snake 
+	}
+
+	/**
+	 * Returns a new model object for the game corresponding to its Name.
+	 * 
+	 * @param gameName
+	 *            The name of the game as given by getGameNames()
+	 * @throws IllegalArgumentException
+	 *             if no such game
+	 */
+	@Override
+	public GameModel createGame(final String gameName) {
+		
+		if (gameName.equals("Gold")) return new GoldModel();
+		else if (gameName.equals("Snake")) return new SnakeModel();// +else if for Snake
+
+		throw new IllegalArgumentException("No such game: " + gameName);
+	}
+}
