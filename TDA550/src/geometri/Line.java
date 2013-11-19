70a5e963a149adee935f72465afb9b0cc3369ce7
Eksell <eksell@student.chalmers.se>
2013-11-19 23:52:10 +0100

    Created Point and Line and made the frame structure for point and as a result also line, files added

---
+++ TDA550/src/geometri/Line.java
@@ -0,0 +1,5 @@
+package geometri;
+
+public class Line {
+
+}
---
+++ TDA550/src/geometri/Point.java
@@ -0,0 +1,44 @@
+package geometri;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Point implements GeometricalForm{
+
+	int x,y,perimiter,width,height,area;
+	Color color = null; 
+
+	public Point(int x, int y, Color c) throws IllegalPositionException{
+		this.color = c;
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point(GeometricalForm f, Color c){
+		//EXTEND	
+	}
+
+	public Color getColor(){	return color;}
+	public int getX(){			return this.x;}
+	public int getY(){			return this.y;}
+	
+	public int getArea(){		return 0;}
+	public int getHeight(){		return 0;}
+	public int getWidth(){		return 0;};
+	public int getPerimeter(){	return 0;}
+	
+	public void move(int dx, int dy){ 
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+	}
+	
+	public void place(int x, int y){ 
+		this.x = x;
+		this.y = y;
+	}
+
+	public void fill(Graphics g){
+		//EXTEND
+	}
+	
+}
