aefc04f8f3d2da4e6d8f3128e9badeb82589f6e7
Eksell <eksell@student.chalmers.se>
2013-12-13 19:35:38 +0100

    Added the Task 3 pack and checked that it works

---
+++ TDA550/Lab3/orig2011/v3/GameModel.java
@@ -0,0 +1,37 @@
+package orig2011.v3;
+
+/**
+ * Common superclass for all game model classes.
+ * 
+ * Constructors of subclasses should initiate matrix elements and additional,
+ * game-dependent fields.
+ */
+
+public interface GameModel {
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 */
+	public GameTile getGameboardState(final Position pos);
+
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param x
+	 *            Coordinate in the gameboard matrix.
+	 * @param y
+	 *            Coordinate in the gameboard matrix.
+	 */
+	public GameTile getGameboardState(final int x, final int y);
+
+
+	/**
+	 * This method is called repeatedly so that the game can update it's state.
+	 * 
+	 * @param lastKey
+	 *            The most recent keystroke.
+	 */
+	public abstract void gameUpdate(int lastKey) throws GameOverException;
+}
